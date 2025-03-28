1f9a11bbb2ac1ec2728582e9c5f5c433cd1045cf
OJ Abba <phavorabba@gmail.com>
2025-03-28 19:53:45 +0000

    Add asset scenes

---
+++ Assets/Scenes/Assets/Scripts/GameManagement/SoundController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+public class SoundController : MonoBehaviour {
+
+    public AudioMixerGroup audioMixer;
+
+	public void PlaySound (AudioSource audioS, AudioClip clip, bool randomizePitch = false, float randomPitchMin = 1, float randomPitchMax = 1) {
+
+		audioS.clip = clip;
+
+		if (randomizePitch == true) {
+			audioS.pitch = Random.Range (randomPitchMin, randomPitchMax);
+		}
+
+		audioS.Play ();
+	}
+
+	public void InstantiateClip (Vector3 pos, AudioClip clip, float time = 2f, bool randomizePitch = false, float randomPitchMin = 1, float randomPitchMax = 1) {
+		GameObject clone = new GameObject ("one shot audio");
+		clone.transform.position = pos;
+		AudioSource audio = clone.AddComponent<AudioSource> ();
+		audio.spatialBlend = 1;
+		audio.volume = 0.5f;
+		audio.clip = clip;
+        audio.outputAudioMixerGroup = audioMixer;
+
+		audio.Play ();
+
+		Destroy (clone, time);
+	}
+}
---
+++ Assets/Scenes/Splash screen/SplashScreen.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SplashScreen : MonoBehaviour
+{
+    public string loadLevel;
+
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(1.5f);
+        SceneManager.LoadScene(loadLevel);
+    }
+}
